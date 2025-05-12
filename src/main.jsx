a76c275ad96e4d0020cc52759071696ff3570bc9
Saranya <76904610+Saranya-IOS@users.noreply.github.com>
2025-05-13 00:17:00 +0530

    Fix CSS imports and setup Tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './css/reset.css'
+import './css/style.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
